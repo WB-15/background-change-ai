04bbf81a329be077c9092726a019d249dd4f79a1
human-pixel <williambronaugh15@gmail.com>
2023-08-29 21:07:40 -0400

    change url

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,12 +12,12 @@ export const metadata = {
   openGraph: {
     title: "Background Changer AI",
     description: "Remove the background from any image with a single click.",
-    url: "https://background-changer-ai.vercel.app",
+    url: "https://background-change-ai.vercel.app",
     locale: "en_US",
     type: "website",
     images: [
       {
-        url: "https://background-changer-ai.vercel.app/app-screenshot.png",
+        url: "https://background-change-ai.vercel.app/app-screenshot.png",
         width: 1200,
         height: 630,
         alt: "Screenshot of the Background Changer AI app",
